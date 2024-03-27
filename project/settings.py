aa7c0efcba008c50d93bf1abaee31282b85eb76a
soash <soash2002@gmail.com>
2024-03-27 18:43:58 +0600

    commit

--- project/settings.py
+++ project/settings.py
@@ -116,11 +116,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR/'staticfiles'
 STATICFILES_DIRS = [BASE_DIR/'static']
 
 
 if PROJECT == 'production':
+    STATIC_ROOT = BASE_DIR/'staticfiles'
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
