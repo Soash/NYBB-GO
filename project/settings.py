e5839489fda3a15dedbc1fc4790593cf3c565694
soash <soash2002@gmail.com>
2024-03-27 20:15:41 +0600

    commit

--- project/settings.py
+++ project/settings.py
@@ -127,15 +127,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-if PROJECT == 'development':
-    STATIC_URL = '/static/'
-    STATICFILES_DIRS = [BASE_DIR/'static']
-    STATIC_ROOT = BASE_DIR/'staticfiles'
-else:
-    STATIC_URL = '/static/'
-    STATICFILES_DIRS = [BASE_DIR/'static']
-    STATIC_ROOT = BASE_DIR/'staticfiles'
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR/'static']
+STATIC_ROOT = BASE_DIR/'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR/'media'
