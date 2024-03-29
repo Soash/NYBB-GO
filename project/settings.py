57babba25e2fc417771a7a47995b04d058387a20
soash <soash2002@gmail.com>
2024-03-30 02:58:00 +0600

    commit

--- project/settings.py
+++ project/settings.py
@@ -141,3 +141,9 @@ MEDIA_ROOT = BASE_DIR/'media'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+# SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True 
+# SECURE_SSL_REDIRECT = True
