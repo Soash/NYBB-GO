dafea6b6aee7794024135720212f905a3a36ee38
soash <soash2002@gmail.com>
2024-03-31 13:22:37 +0600

    commit

--- app/views.py
+++ app/views.py
@@ -18,19 +18,20 @@ def quiz1(request):
     if request.method == 'POST':
         submitted_code = request.POST.get('code')
         if submitted_code == '2504':
-            if hasattr(request.user, 'team'):  # Check if the user has an associated team
+            if hasattr(request.user, 'team'):
                 team = request.user.team
                 if team.quiz_1_status == False:
                     team.score += 100 
                 team.quiz_1_status = True
-                team.save() 
+                team.save()
+                return redirect(quiz2) 
 
     return render(request, 'quiz1.html')
 
 
 
 def quiz2(request):
-    if request.user.is_authenticated and request.user.team.quiz_1_status == True:
+    if request.user.team.quiz_1_status == True:
         if request.method == 'POST':
             submitted_code = request.POST.get('geneL')
             if submitted_code == '12q23.2':
@@ -39,7 +40,8 @@ def quiz2(request):
                     if team.quiz_2_status == False:
                         team.score += 100 
                     team.quiz_2_status = True
-                    team.save() 
+                    team.save()
+                    return redirect(quiz3) 
 
         return render(request, 'quiz2.html')
   
@@ -49,7 +51,7 @@ def quiz2(request):
 
 @login_required
 def quiz3(request):
-    if request.user.is_authenticated and request.user.team.quiz_2_status == True:
+    if request.user.team.quiz_2_status == True:
         if request.method == 'POST':
             submitted_code = request.POST.get('UnlockKey')
             if submitted_code == '439':
@@ -59,6 +61,7 @@ def quiz3(request):
                         team.score += 100
                     team.quiz_3_status = True
                     team.save()
+                    return redirect(quiz4)
 
         return render(request, 'quiz3.html')
     
@@ -66,44 +69,47 @@ def quiz3(request):
 
 @login_required
 def quiz4(request):
-    if request.method == 'POST':
-        submitted_code = request.POST.get('UnlockKey')
-        if submitted_code == '6666':
-            if hasattr(request.user, 'team'):
-                team = request.user.team
-                if team.quiz_4_status == False:
-                    team.score += 100
-                team.quiz_4_status = True
-                team.save()
-                return redirect(index)
-    team = request.user.team
-    team.score -= 1
-    team.save()
-    return render(request, 'quiz4.html')
+    if request.user.team.quiz_3_status == True:
+        if request.method == 'POST':
+            submitted_code = request.POST.get('UnlockKey')
+            if submitted_code == '6666':
+                if hasattr(request.user, 'team'):
+                    team = request.user.team
+                    if team.quiz_4_status == False:
+                        team.score += 100
+                    team.quiz_4_status = True
+                    team.save()
+                    return redirect(quiz5)
+                
+        team = request.user.team
+        team.score -= 1
+        team.save()
+        return render(request, 'quiz4.html')
+    
+    return redirect(quiz3)
 
 @login_required
 def quiz5(request):
-    if request.method == 'POST':
-        submitted_code = request.POST.get('UnlockKey').lower()
-        if submitted_code == 'crispr':
-            if hasattr(request.user, 'team'):
-                team = request.user.team
-                if team.quiz_5_status == False:
-                    team.score += 100
-                team.quiz_5_status = True
-                team.save()
-                return redirect(index)
-    return render(request, 'quiz5.html')
+    if request.user.team.quiz_4_status == True:
+        if request.method == 'POST':
+            submitted_code = request.POST.get('UnlockKey').lower()
+            if submitted_code == 'crispr':
+                if hasattr(request.user, 'team'):
+                    team = request.user.team
+                    if team.quiz_5_status == False:
+                        team.score += 100
+                    team.quiz_5_status = True
+                    team.save()
+                    return redirect(index)
+        return render(request, 'quiz5.html')
+    
+    return redirect(quiz4)
 
 
 def update_score(request):
     team = request.user.team
     team.score -= 10
     team.save()
-    # Your logic to update the user's score goes here
-    # For example, you can access the user's score from request.user.team.score
-    
-    # Return a JSON response indicating success or failure
     return JsonResponse({'status': 'success'})
 
 
